bf563263e7d23d0b6707cbc2688d991b60cfa28e
filipkulasinac <95426329+filipkulasinac@users.noreply.github.com>
2021-12-16 15:30:33 +0100

    created navigation bar and fixed bug with actionbutton (#13)

---
+++ utils/navLinks.js
@@ -0,0 +1,15 @@
+export const navLinks = [
+  { name: "Home", path: "/" },
+  {
+    name: "Characters",
+    path: "/characters",
+  },
+  {
+    name: "Locations",
+    path: "/locations",
+  },
+  {
+    name: "Episodes",
+    path: "/episodes",
+  },
+];

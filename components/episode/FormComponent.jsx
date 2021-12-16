81ec35bd542f059799247b3118bacf3e8e4db702
milan <milan@milan.com>
2021-12-16 17:24:39 +0100

    Formik& Yup implemented in episode create/edit functionality. WIP

--- components/episode/FormComponent.jsx
+++ components/episode/FormComponent.jsx
@@ -1,109 +1,115 @@
-import { useState } from "react";
 import Button from "react-bootstrap/Button";
-import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
 import Link from "next/link";
-import useEpisodesFormData from "/hooks/useEpisodesFormData";
+import { useFormik } from "formik";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import * as Yup from "yup";
 
 function FormComponent({ submitHandler, initialData }) {
-  const { episodeData, setEpisodeData } = useEpisodesFormData(initialData);
-
-  const [visitedName, setVisitedName] = useState(false);
-  const [visitedAirDate, setVisitedAirDate] = useState(false);
-  const [visitedEpisodeDesc, setVisitedEpisodeDesc] = useState(false);
-  const [episodeDescValid, setEpisodeDescValid] = useState(true);
-
-  const isFormValid =
-    episodeData.name &&
-    episodeData.air_date &&
-    episodeData.episodeDesc &&
-    episodeDescValid;
-
-  const handleChange = (e, propName) =>
-    setEpisodeData((prev) => ({ ...prev, [propName]: e.target.value }));
-
-  function submitFunction(e) {
-    e.preventDefault();
-    submitHandler(episodeData);
+  function submitFunction(submittedEpisodeData) {
+    submittedEpisodeData.air_data = submittedEpisodeData.airDate;
+    delete submittedEpisodeData.airDate;
+    submitHandler(submittedEpisodeData);
   }
 
-  function checkEpisodeDesc(episodeDesc) {
-    return /^S[1-9][0-9]E[1-9][0-9]$/.test(episodeDesc);
-  }
+  const episodeSchema = Yup.object({
+    name: Yup.string()
+      .min(4, "Must be 4 characters or more")
+      .required("Required amigo"),
+    airDate: Yup.string("Date error").required("Required"),
+    episodeDesc: Yup.string()
+      .matches(
+        /^S[0-9][1-9]E[0-9][1-9]$/,
+        "Please enter an episode in proper format S01E01"
+      )
+      .required("Required"),
+  });
+
+  const initialValues = {
+    name: initialData.name,
+    airDate: initialData.air_date,
+    episodeDesc: initialData.episodeDesc,
+  };
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: episodeSchema,
+    onSubmit: submitFunction,
+  });
 
   return (
-    <form onSubmit={submitFunction} noValidate>
+    <Form noValidate onSubmit={formik.handleSubmit}>
       <div className="flex flex-col gap-y-2">
-        <div className={visitedName ? "was-validated" : ""}>
-          <FormControl
-            aria-label="Default"
-            aria-describedby="inputGroup-sizing-default"
-            placeholder="Name"
-            value={episodeData.name}
-            onChange={(e) => handleChange(e, "name")}
-            onBlur={() => {
-              setVisitedName(true);
-            }}
-            required
+        <FloatingLabel controlId="floatingInput" label="Name" className="mb-3">
+          <Form.Control
+            name="name"
+            type="text"
+            className="input form-control"
+            placeholder="Episode Name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            isInvalid={formik.touched.name && formik.errors.name}
           />
-          <div className="invalid-feedback">
-            {!episodeData.name
-              ? "Please enter a name!"
-              : episodeData.name.length < 3
-              ? "Name must have at least three characters!"
-              : ""}
-          </div>
-        </div>
-        <div className={visitedAirDate ? "was-validated" : ""}>
-          <FormControl
-            aria-label="Default"
-            aria-describedby="inputGroup-sizing-default"
-            placeholder="Air Date"
-            value={episodeData.air_date}
-            onChange={(e) => handleChange(e, "air_date")}
-            onBlur={() => setVisitedAirDate(true)}
-            required
-            type="date"
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.name}
+          </Form.Control.Feedback>
+        </FloatingLabel>
+
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Air date"
+          className="mb-3"
+        >
+          <Form.Control
+            name="airDate"
+            type="text"
+            className="input form-control"
+            placeholder="Air date"
+            value={formik.values.airDate}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            isInvalid={formik.touched.airDate && formik.errors.airDate}
           />
-          <div className="invalid-feedback">Please enter an Air Date!</div>
-        </div>
-        <div className={visitedEpisodeDesc ? "was-validated" : ""}>
-          <FormControl
-            aria-label="Default"
-            aria-describedby="inputGroup-sizing-default"
-            placeholder="Episode"
-            value={episodeData.episodeDesc}
-            onChange={(e) => handleChange(e, "episodeDesc")}
-            onBlur={() => {
-              setVisitedEpisodeDesc(true);
-              setEpisodeDescValid(checkEpisodeDesc(episodeData.episodeDesc));
-            }}
-            required
-            pattern="^S[1-9][0-9]E[1-9][0-9]$"
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.airDate}
+          </Form.Control.Feedback>
+        </FloatingLabel>
+
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Episode Description"
+          className="mb-3"
+        >
+          <Form.Control
+            name="episodeDesc"
+            type="text"
+            className="input form-control"
+            placeholder="Episode Description"
+            value={formik.values.episodeDesc}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            isInvalid={formik.touched.episodeDesc && formik.errors.episodeDesc}
           />
-          <div className="invalid-feedback">
-            {!episodeData.episodeDesc
-              ? "Please enter an Episode Short name!"
-              : ""}
-            {!episodeDescValid
-              ? "Please enter an episode in proper format S01E01!"
-              : ""}
-          </div>
-        </div>
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.episodeDesc}
+          </Form.Control.Feedback>
+        </FloatingLabel>
+
         <div className="flex flex-row p-2">
           <Link href="/episodes">
             <Button variant="btn btn-outline-danger  w-1/2 mr-2">Back</Button>
           </Link>
-
           <Button
             variant="btn btn-outline-success w-1/2"
             type="submit"
-            disabled={!isFormValid}
+            disabled={!formik.isValid}
           >
             {!initialData ? "Add new episode!" : "Update episode"}
           </Button>
         </div>
       </div>
-    </form>
+    </Form>
   );
 }
 export default FormComponent;

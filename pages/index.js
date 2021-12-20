e4cafb79ac895dd179b8f41f4cecc1089c70fce1
filipkulasinac <95426329+filipkulasinac@users.noreply.github.com>
2021-12-20 14:18:05 +0100

    added background and styling on home page (#17)
    * added background and styling on home page
    
    * removed relative
    
    Co-authored-by: Bojan Crevar <bojan.crevar@hpe.com>

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
 import React from "react";
-import Link from "next/link";
 
+const myStyle = {
+  background: "url(rickandmorty.png)",
+  opacity: "0.2",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPositionX: "center",
+  backgroundPositionY: "center",
+  backgroundAttachment: "fixed",
+  height: "100%",
+  width: "100%",
+  position: "absolute",
+  top: 0,
+  left: 0,
+};
 const HomePage = () => {
   return (
-    <div className="mx-auto w-3/4 mt-1">
-      <h4 className="text-center">Welcome to Rick and Morty app</h4>
-      <div className="my-10">
-        Select link:
-        <ul className="list-disc">
-          <li>
-            <Link href="/locations">Locations</Link>
-          </li>
-          <li>
-            <Link href="/episodes">Episodes</Link>
-          </li>
-          <li>
-            <Link href="/characters">Characters</Link>
-          </li>
-        </ul>
+    <div className="flex-1 relative">
+      <div style={myStyle}></div>
+      <div className="text-center">
+        <h1 className="lg:text-6xl lg:pt-48 md:text-4xl md:pt-32 sm:text-2xl sm:pt-20 opacity-60">
+          Welcome to the Rick and Morty app
+        </h1>
       </div>
     </div>
   );

74655382edf38671ddba3554d68cf191d19d5d4a
Milan Nikolic <azurepopcorn@gmail.com>
2021-12-21 11:09:02 +0100

    Make page relodable feature done (#18)
    Co-authored-by: milan <milan@milan.com>

--- pages/episodes/index.js
+++ pages/episodes/index.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import EpisodeList from "../../components/EpisodeList";
 import axios from "axios";
 import Pagination from "../../components/Pagination";
 import Searchbar from "../../components/Searchbar";
 import Link from "next/link";
 import Button from "react-bootstrap/Button";
+import { useRouter } from "next/router";
 
-const EpisodesPage = () => {
+const EpisodesPage = (props) => {
+  const router = useRouter();
+
   const [episodes, setEpisodes] = useState();
   const [pagesInfo, setPagesInfo] = useState({});
-  const [activePage, setActivePage] = useState(1);
-  const [keyword, setKeyword] = useState();
+  const [activePage, setActivePage] = useState(+props?.query?.activePage || 1);
+  const [keyword, setKeyword] = useState(props?.query?.keyword || "");
 
   async function fetchData() {
     const response = await axios.get("/api/episodes", {
@@ -22,18 +25,13 @@ const EpisodesPage = () => {
   }
 
   useEffect(() => {
-    if (activePage !== 1) {
-      setActivePage(1);
-    } else {
-      fetchData();
-    }
-  }, [keyword]);
-
-  useEffect(() => {
     fetchData();
-  }, [activePage]);
+    const keywordQuery = keyword ? `&keyword=${keyword}` : "";
+    router.push(`?activePage=${activePage}${keywordQuery}`, undefined, {
+      shallow: true,
+    });
+  }, [activePage, keyword]);
 
-  console.log("episodes", episodes);
   return (
     <div className="m-auto w-1/2 ">
       <h5 className="p-4 text-4xl	text-center">Rick & Morty list of episodes</h5>
@@ -43,7 +41,11 @@ const EpisodesPage = () => {
         setActivePage={setActivePage}
       />
       <div>Pages: </div>
-      <Searchbar setKeyword={setKeyword} setActivePage={setActivePage} />
+      <Searchbar
+        setKeyword={setKeyword}
+        initKeyword={keyword}
+        setActivePage={setActivePage}
+      />
       <div className="pt-4">
         <Link href="/episodes/create">
           <Button variant="success w-1/2" type="submit">
@@ -57,5 +59,9 @@ const EpisodesPage = () => {
     </div>
   );
 };
+
+export async function getServerSideProps({ query }) {
+  return { props: { query: query || null } };
+}
 
 export default EpisodesPage;

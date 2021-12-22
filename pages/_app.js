7c117149f69d1c95b9cd2cf6c9ddbbf91942c81c
milan <milan@milan.com>
2021-12-22 16:47:43 +0100

    Spinner implementation

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../_styles/App.css";
 
 import NavMenu from "../components/NavMenu";
+import LoaderOverlay from "../components/LoaderOverlay";
+import OverlayContextProvider from "../context/OverlayContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="bg-gray-400 h-full">
       <NavMenu />
-      <div
-        className="flex overflow-auto"
-        style={{ height: "calc(100% - 56px)" }}
-      >
-        <Component {...pageProps} />
-      </div>
+      <OverlayContextProvider>
+        <LoaderOverlay />
+        <div
+          className="flex overflow-auto"
+          style={{ height: "calc(100% - 56px)" }}
+        >
+          <Component {...pageProps} />
+        </div>
+      </OverlayContextProvider>
     </div>
   );
 }

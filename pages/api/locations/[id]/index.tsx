f3e3543f953129e65e2e920113f4b5d0ca576c50
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-01-03 13:58:08 +0100

    tsx pages with some errors. need to fix them

---
+++ pages/api/locations/[id]/index.tsx
@@ -0,0 +1,40 @@
+import locationsRepo from "../../../../utils/locations-repo";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      {
+        let { id } = req.query;
+        if (!id) {
+          res.status(404).json({ error: "No location!" });
+        }
+        const location = locationsRepo.getById(id);
+        res.status(200).json({
+          location: location,
+        });
+      }
+      break;
+    case "PUT":
+      {
+        const body = req.body;
+        locationsRepo.update(body);
+
+        res.status(200).json("success");
+      }
+      break;
+    case "DELETE":
+      {
+        let { id } = req.query;
+        locationsRepo.delete(id);
+
+        res.status(200).json("success");
+      }
+      break;
+    default:
+      break;
+  }
+}

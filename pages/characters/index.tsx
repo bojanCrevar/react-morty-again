c30481cee0077c3cf3893ef53e175b31b89507a0
FlorjanKlancar <48727773+FlorjanKlancar@users.noreply.github.com>
2022-01-04 13:53:46 +0100

    Updated file because of wrong commit

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Button from "react-bootstrap/Button";
 import CharacterList from "../../components/CharacterList";
@@ -10,7 +10,6 @@ import { useRouter } from "next/router";
 import CharactersSkeleton from "../../components/skeletons/CharactersSkeleton";
 import { GetServerSidePropsContext } from "next/types";
 import { ResponseData } from "../../model/charactersModel";
-import FilterPanel from "../../components/FilterPanel";
 
 function Characters() {
   const router = useRouter();
@@ -42,77 +41,36 @@ function Characters() {
     );
   }, [activePage, keyword, sort]);
 
-  const filterConfig = [
-    {
-      title: "Gender",
-      values: ["Male", "Female", "Genderless"],
-      type: "checkbox",
-    },
-    {
-      title: "Testing",
-      values: ["1", "2", "3"],
-      type: "radio",
-    },
-    {
-      title: "Status",
-      values: ["Dead", "Alive", "Unknown"],
-      type: "checkbox",
-    },
-    {
-      title: "Status2",
-      values: ["Dead", "Alive", "Unknown"],
-      type: "radio",
-    },
-  ];
-
-  function submitHandler(e: any) {
-    e.preventDefault();
-    console.log("Submit");
-  }
-
   return (
-    <div className="flex mb-4 w-full">
-      <div className="w-1/4">
-        <div className="w-1/2 ml-28 mt-44">
-          <FilterPanel
-            filterConfig={filterConfig}
-            submitHandler={submitHandler}
-            date={true}
-          />
-        </div>
+    <div className="m-auto w-3/4 2xl:w-1/2">
+      <h5 className="p-4 text-4xl	text-center">
+        List of characters - {pagesInfo.count}
+      </h5>
+      <Pagination
+        pagesInfo={pagesInfo}
+        activePage={activePage}
+        setActivePage={setActivePage}
+      />
+      <div>Pages: {pagesInfo.pages}</div>
+      <Searchbar
+        setKeyword={setKeyword}
+        setActivePage={setActivePage}
+        initKeyword={keyword}
+      />
+      <div className="pt-4 relative">
+        <Link href="characters/create">
+          <Button variant="success w-1/2" type="submit">
+            Add character!
+          </Button>
+        </Link>
+        <SortComponent setSort={setSort} initSort={sort} />
       </div>
-      <div className="w-2/4 ">
-        <div>
-          <h5 className="p-4 text-4xl	text-center">
-            List of characters - {pagesInfo.count}
-          </h5>
-          <Pagination
-            pagesInfo={pagesInfo}
-            activePage={activePage}
-            setActivePage={setActivePage}
-          />
-          <div>Pages: {pagesInfo.pages}</div>
-          <Searchbar
-            setKeyword={setKeyword}
-            setActivePage={setActivePage}
-            initKeyword={keyword}
-          />
-          <div className="pt-4 relative">
-            <Link href="characters/create">
-              <Button variant="success w-1/2" type="submit">
-                Add character!
-              </Button>
-            </Link>
-            <SortComponent setSort={setSort} initSort={sort} />
-          </div>
-          <div className="mt-8">
-            {chars.length ? (
-              <CharacterList characters={chars} fetchData={fetchData} />
-            ) : (
-              <CharactersSkeleton amount={10} />
-            )}
-          </div>
-        </div>
+      <div className="mt-8">
+        {chars ? (
+          <CharacterList characters={chars} fetchData={fetchData} />
+        ) : (
+          <CharactersSkeleton amount={10} />
+        )}
       </div>
     </div>
   );

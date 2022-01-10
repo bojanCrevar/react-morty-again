af84833efe1a7ad8e2acad3a1d857f1c7d6dd5f8
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-01-10 15:39:50 +0100

    Added searchbar test

---
+++ components/Searchbar.test.js
@@ -0,0 +1,45 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Searchbar from "./Searchbar";
+
+describe("Searchbar component test", () => {
+  test("render Searchbar on screen", () => {
+    let setKeyword;
+    let setActivePage;
+    let activePage;
+    let keyword;
+
+    keyword = "Rick";
+    setActivePage = (activeArg) => (activePage = activeArg);
+
+    setKeyword = (keyword) => {
+      keyword = keyword;
+
+      rerender(
+        <Searchbar
+          setKeyword={setKeyword}
+          setActivePage={setActivePage}
+          initKeyword={keyword}
+        />
+      );
+    };
+
+    const { rerender } = render(
+      <Searchbar
+        setKeyword={setKeyword}
+        setActivePage={setActivePage}
+        initKeyword={keyword}
+      />
+    );
+
+    const buttonElement = screen.getByRole("button", {
+      name: "Search",
+    });
+
+    fireEvent.click(buttonElement);
+
+    const outputElem = screen.getByDisplayValue("Rick");
+    expect(outputElem).toBeInTheDocument();
+  });
+});

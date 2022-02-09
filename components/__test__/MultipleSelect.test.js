358bba74ca496023a076b110f9dcffdd0dc4d9ed
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-02-09 15:29:41 +0100

    multiselect tests

--- components/__test__/MultipleSelect.test.js
+++ components/__test__/MultipleSelect.test.js
@@ -1,8 +1,12 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import MultipleSelect from "../MultipleSelect";
 import axios from "axios";
+import mockAxios from "jest-mock-axios";
 
-jest.mock("axios");
+afterEach(() => {
+  // cleaning up the mess left behind the previous test
+  mockAxios.reset();
+});
 
 describe("Testing MultipleSelect", () => {
   test("rendering multiselect", () => {
@@ -11,35 +15,76 @@ describe("Testing MultipleSelect", () => {
       "https://rickandmortyapi.com/api/character/38",
       "https://rickandmortyapi.com/api/character/45",
     ];
-
     onChange = jest.fn();
 
     render(
       <MultipleSelect name="characters" onChange={onChange} value={value} />
     );
 
-    screen.debug();
-
     const multiSelect = screen.getByRole("combobox");
 
     expect(multiSelect).toBeInTheDocument();
   });
 
-  it("testing backend call", async () => {
-    // given
-    const users = [
-      { id: 1, name: "John" },
-      { id: 2, name: "Andrew" },
-    ];
-    axios.get.mockResolvedValueOnce(users);
+  describe("fetchData", () => {
+    it("should return users list", async () => {
+      mockAxios.get.mockResolvedValueOnce(value);
 
-    // when
-    const result = await fetchUsers();
+      render(
+        <MultipleSelect name="characters" onChange={onChange} value={value} />
+      );
 
-    // then
-    expect(axios.get).toHaveBeenCalled();
-    //expect(result).toEqual(users);
+      screen.debug();
+    });
   });
+
+  describe("check onChange trigger", () => {
+    it("should trigger onChange fn", async () => {
+      let onChange;
+      let value = [
+        "https://rickandmortyapi.com/api/character/38",
+        "https://rickandmortyapi.com/api/character/45",
+      ];
+
+      onChange = jest.fn();
+
+      render(
+        <MultipleSelect name="characters" onChange={onChange} value={value} />
+      );
+
+      const multiSelect = screen.getByRole("combobox");
+
+      fireEvent.change(multiSelect, {
+        target: { value: "https://rickandmortyapi.com/api/character/38" },
+      });
+
+      expect(onChange).toHaveBeenCalled();
+    });
+  });
+
+  /*  describe("add filter input", () => {
+    it("should trigger backend filtering", async () => {
+      let onChange;
+      let value = [
+        "https://rickandmortyapi.com/api/character/38",
+        "https://rickandmortyapi.com/api/character/45",
+      ];
+
+      onChange = jest.fn();
+
+      render(
+        <MultipleSelect name="characters" onChange={onChange} value={value} />
+      );
+
+      const multiSelect = screen.getByRole("combobox");
+
+      fireEvent.change(multiSelect, {
+        target: { value: "Rick" },
+      });
+
+      expect(onChange).toHaveBeenCalled();
+    });
+  }); */
 });
 
 //check if onchange is called on selection Change

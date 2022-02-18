e443d2537b68f057e66764522e032f124394a0f8
bojanCrevar <59404276+bojanCrevar@users.noreply.github.com>
2022-02-18 15:25:11 +0100

    Bcrevar keyword page switch fix (#75)
    * Keyword page switch fix
    
    * Keyword page switch fix
    
    Co-authored-by: Bojan Crevar <bojan.crevar@hpe.com>

--- pages/_app.js
+++ pages/_app.js
@@ -2,25 +2,30 @@ import "tailwindcss/tailwind.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../_styles/App.css";
 import "react-loading-skeleton/dist/skeleton.css";
-import { useEffect } from "react";
+import { useEffect, useRef} from "react";
 import { useRouter } from "next/router";
 import store from "../store";
-import { Provider, useDispatch, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import NavMenu from "../components/NavMenu.tsx";
 import { filterActions } from "../store/filter-slice";
 import { fetchUserOnReload } from "../store/auth-actions";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
+  const resetQuery = useRef(false);
 
   useEffect(() => {
     const handleRouteChange = (url, { shallow }) => {
       if (!shallow) {
-        store.dispatch(filterActions.resetKeywordandFilter());
+        resetQuery.current = true;
       }
     };
     router.events.on("routeChangeStart", handleRouteChange);
   }, []);
+  if(resetQuery.current) {
+    resetQuery.current = false;
+    store.dispatch(filterActions.resetKeywordandFilter());
+  }
 
   useEffect(() => {
     store.dispatch(fetchUserOnReload());
